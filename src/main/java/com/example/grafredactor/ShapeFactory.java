20cac6b5d7d181574888025e3a89f59f60c5ce28
a-chernousov <andreu04032004@yandex.ru>
2024-12-11 18:00:44 +0300

    updated and new localization added

--- src/main/java/com/example/grafredactor/ShapeFactory.java
+++ src/main/java/com/example/grafredactor/ShapeFactory.java
@@ -6,11 +6,15 @@ public class ShapeFactory {
     public static Shape createShape(String type, double x, double y, double size, Color color) {
         switch (type) {
             case "Круг":
+            case "Circle":
                 return new Circle(x, y, size, color);
             case "Квадрат":
+            case "Square":
                 return new Rectangle(x, y, size, size, color);
             case "Треугольник":
+            case "Triangle":
                 return new Triangle(x, y, size, color);
+            case "Point":
             case "Точка":
                 return new Points(x, y, size, color);
             default:
